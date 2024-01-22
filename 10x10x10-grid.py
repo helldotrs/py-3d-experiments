3ff1102c9fec22fb5cc60065d2d504735f1c1b01
helldotrs <macrosystem@proton.me>
2024-01-23 00:28:49 +0100

    removing hardcoded size

--- 10x10x10-grid.py
+++ 10x10x10-grid.py
@@ -1,6 +1,11 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+# Define the range for each axis
+x_range = 10
+y_range = 10
+z_range = 10
+
 # Create a new figure
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -8,9 +13,9 @@ ax = fig.add_subplot(111, projection='3d')
 # Generate grid points and annotations
 x, y, z, s = [], [], [], []
 i = 0
-for ix in range(10):
-    for iy in range(10):
-        for iz in range(10):
+for ix in range(x_range):
+    for iy in range(y_range):
+        for iz in range(z_range):
             x.append(ix)
             y.append(iy)
             z.append(iz)
@@ -21,17 +26,4 @@ for ix in range(10):
             i += 1
 
 # Plot the points
-scatter = ax.scatter(x, y, z)
-
-# Annotate each point
-for i in range(len(x)):
-    if s[i]:  # Only annotate if the string is not empty
-        ax.text(x[i], y[i], z[i], s[i], size=10, zorder=1, color='k')
-
-# Set labels
-ax.set_xlabel('X Axis')
-ax.set_ylabel('Y Axis')
-ax.set_zlabel('Z Axis')
-
-# Show the plot
-plt.show()
+scatter = ax.scatter(x, y,
